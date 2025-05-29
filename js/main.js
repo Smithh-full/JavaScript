ba9e22142a35b19895521fefe535933875d81a8d
Smithh <esfuentess@libertadores.edu.co>
2025-05-29 00:21:10 +0000

    Ni monda

--- js/main.js
+++ js/main.js
@@ -1,5 +1,1 @@
-// Script principal de Caperucita Roja
-window.addEventListener('load', () => {
-    console.log('Aplicación de Caperucita Roja iniciada');
-    // Aquí inicializaremos las animaciones de Mo.js cuando las necesitemos
-});
+// Archivo vacío según solicitud del usuario
